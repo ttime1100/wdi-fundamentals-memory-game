5290165529817db911d244ee957305bca96487e4
ttime1100 <ttime1100@gmail.com>
2018-01-11 23:55:36 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and Running");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
