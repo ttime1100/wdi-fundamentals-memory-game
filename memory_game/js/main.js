e3d36a6d9d4d5921faad4b00e5674e72eb061204
ttime1100 <ttime1100@gmail.com>
2018-01-12 01:41:14 -0800

    game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,54 @@
 console.log("Up and Running");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardTwo);
+var cards =[
+  {
+    rank:"king",
+    suit:"hearts",
+    cardImage:"images/king-of-hearts.png"
+  },
+{
+  rank:"king",
+  suit:"diamonds",
+  cardImage:"images/king-of-diamonds.png"
+},
+  {
+    rank:"queen",
+    suit:"hearts",
+    image:"images/queen-of-hearts.png"
+  },
+{
+    rank:"queen",
+    suit:"diamonds",
+    image:"images/queen-of-diamonds.png"
+  }
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+}
+else {
+console.log("Sorry, try again.");
+}
+}
+
+var flipCard = function(cardsId) {
+
+  checkForMatch();
+
+  console.log("User Flipped " + cards[cardsId].rank);
+  cardsInPlay.push(cards[cardsId].rank);
+
+  if (cardsInPlay.length === 2) {
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+     alert("You found a match");
+   }
+   else {
+     alert("Sorry, Try again");
+   }
+};
+console.log(cards.cardImage);
+C:\Users\T-Time\fundamentals\memory_game
